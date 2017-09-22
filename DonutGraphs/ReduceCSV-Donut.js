f27a8a9a1ab4b14c30fe8e0b525998623381875f
Bryan Garcia <BryanGarcia831@gmail.com>
2017-09-22 15:05:57 -0700

    Added js for calc donut. json's added.

---
+++ DonutGraphs/ReduceCSV-Donut.js
@@ -0,0 +1,53 @@
+
+let uniqueCategory = {};
+let uniqueDistricts = {};
+let uniqueResolutions = {};
+
+
+function reduceCSV() {
+	d3.csv('../data/Police_Department_Incidents.csv', function (file) {
+		file.forEach(function (row) {
+
+			// Unique Categories
+			if (!uniqueCategory[row.Category]) {
+				uniqueCategory[row.Category] = 1;
+			} else {
+				uniqueCategory[row.Category]++;
+			}
+
+			// Unique Districts
+			if (!uniqueDistricts[row.PdDistrict]) {
+				uniqueDistricts[row.PdDistrict] = 1;
+			} else {
+				uniqueDistricts[row.PdDistrict]++;
+			}
+
+			// Unique Resolutions
+			if (!uniqueResolutions[row.Resolution]) {
+				uniqueResolutions[row.Resolution] = 1;
+			} else {
+				uniqueResolutions[row.Resolution]++;
+			}
+
+
+
+		});
+
+
+		saveText(JSON.stringify(uniqueDistricts), "uniqueDistricts.json");
+		saveText(JSON.stringify(uniqueCategory), "uniqueCategory.json");
+		saveText(JSON.stringify(uniqueResolutions), "uniqueResolutions.json");
+	});
+
+
+}
+
+
+function saveText(text, filename){
+	let a = document.createElement('a');
+	a.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(text));
+	a.setAttribute('download', filename);
+	a.click()
+}
+
+reduceCSV();
